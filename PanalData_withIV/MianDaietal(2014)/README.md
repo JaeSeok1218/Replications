e762d3ce61ad5c32986aafb09aab62c8d29e3f31
JaeSeok <dhwotjr7755@gmail.com>
2024-02-10 22:14:43 -0600

    many

--- PanalData_withIV/MianDaietal(2014)/README.md
+++ PanalData_withIV/MianDaietal(2014)/README.md
@@ -1,3 +1,8 @@
 ### Table 1
 
 [<img src="./Results/Table1.png" title="Table 1" alt="Table 1" width="400.3" height="231"/>](./Table1.R)
+
+
+### Table 2
+
+[<img src="./Results/Table2.png" title="Table 2" alt="Table 2" width="400.3" height="231"/>](./Table2.R)
--- PanalData_withIV/MianDaietal(2014)/Table2.R
+++ PanalData_withIV/MianDaietal(2014)/Table2.R
@@ -1,9 +1,14 @@
 text_tbl <- data.frame(
   Variable = c("lasset", "lasset$^{2}$", "cash", "opexp", "otherinc", "bankr"),
   Definition = c("Logged total assets", "Lasset squared", "Cash available", "Operating expenses", "Nonoperating income", "Bankruptcy indicator"),
-  Mean = c(mean(data_ap$lasset), mean(data_ap$lass_sq), mean(data_ap$cash), mean(data_ap$op_expenses), mean(data_ap$non_op_income), mean(data_ap$bankr)),
-  SD = c(sd(data_ap$lasset), sd(data_ap$lass_sq), sd(data_ap$cash), sd(data_ap$op_expenses), sd(data_ap$non_op_income), sd(data_ap$bankr))
+  Mean = c(round(mean(data_ap$lasset), digits=3), round(mean(data_ap$lass_sq), digits=3), 
+           round(mean(data_ap$cash), digits=3), round(mean(data_ap$op_expenses), digits=3), 
+           round(mean(data_ap$non_op_income), digits=3), round(mean(data_ap$bankr), digits=3)),
+  SD = c(round(sd(data_ap$lasset), digits=3), round(sd(data_ap$lass_sq), digits=3), 
+         round(sd(data_ap$cash), digits=3), round(sd(data_ap$op_expenses), digits=3), 
+         round(sd(data_ap$non_op_income), digits=3), round(sd(data_ap$bankr), digits=3))
 )
 
-kbl(text_tbl, booktabs = T, 'latex') %>%
-  kable_styling(full_width = )
+kbl(text_tbl,align = "c", caption = "CONTROL VARIABLES" , booktabs = T, 'latex') %>%
+  kable_styling(full_width = ) %>%
+  footnote(general = "Number of observations is 248,513. cash, opexp, and otherinc are computed as percentage of total assets", threeparttable = T)
